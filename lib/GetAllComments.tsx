75a4330acc4a9d7c4980aa8672345b61f07f8238
Jeremy84100 <96269524+Jeremy84100@users.noreply.github.com>
2023-10-15 20:49:04 +0200

    Supplies added

--- lib/GetAllComments.tsx
+++ lib/GetAllComments.tsx
@@ -3,7 +3,7 @@ const maxResults = 20;
 
 export default async function GetAllComments(videoId: string) {
   const res = await fetch(
-    `https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&order=relevance&videoId=${videoId}&key=AIzaSyBXhPOc1wSu_Zzj7NKDO3RX6FymyhhHAiw`
+    `https://www.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&order=relevance&videoId=${videoId}&key=AIzaSyBXhPOc1wSu_Zzj7NKDO3RX6FymyhhHAiw`
     );
 
   if (!res.ok) throw new Error("failed to fetch data");
