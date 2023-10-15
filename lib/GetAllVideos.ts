ef85b0a710b6e7c16326c319002d08de31b7281b
Jeremy84100 <96269524+Jeremy84100@users.noreply.github.com>
2023-10-15 20:42:58 +0200

    fix error comments

--- lib/GetAllVideos.ts
+++ lib/GetAllVideos.ts
@@ -2,7 +2,7 @@ const apiKey = process.env.YOUTUBE_API_KEY;
 const maxResults = 15;
 
 export default async function GetAllVideos() {
-    const res = await fetch(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyBXhPOc1wSu_Zzj7NKDO3RX6FymyhhHAiw&part=snippet&type=video&maxResults=${maxResults}`)
+    const res = await fetch(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyBXhPOc1wSu_Zzj7NKDO3RX6FymyhhHAiw&part=snippet&type=video&maxResults=${maxResults}&order=relevance&eventType=completed&q=ReactJs`)
 
     if (!res.ok) throw new Error('failed to fetch data')
 
