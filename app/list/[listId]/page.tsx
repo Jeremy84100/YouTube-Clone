a1ab9cbe36ff668a4f59c7cee41d99b075bf5113
Jeremy84100 <96269524+Jeremy84100@users.noreply.github.com>
2023-11-17 01:43:53 +0100

    list page finish

--- app/list/[listId]/page.tsx
+++ app/list/[listId]/page.tsx
@@ -6,7 +6,14 @@ import GetVideo from "@/lib/GetVideo";
 import Image from "next/image";
 import { formatDate } from "@/lib/FormatCount";
 import Link from "next/link";
-import { Play } from "lucide-react";
+import {
+  ArrowDownToLine,
+  Forward,
+  ListPlus,
+  MoreVertical,
+  Play,
+  Shuffle,
+} from "lucide-react";
 
 export default async function List({ params }: { params: { listId: string } }) {
   const listData = GetPlaylistItems(params.listId);
@@ -30,11 +37,11 @@ export default async function List({ params }: { params: { listId: string } }) {
   return (
     <div className="flex pt-6">
       <div>
-        <div className="fixed flex h-playlist pb-9 flex-col p-6 rounded-2xl bg-slate-600 w-22.5">
+        <div className="fixed h-playlist p-6 rounded-2xl bg-slate-600 w-22.5">
           <Link
             href={`/video/${videoInfo.items[0].id}`}
             passHref
-            className="group flex flex-col mb-10 items-center">
+            className="group flex flex-col mb-6 items-center">
             <div className="relative rounded-lg overflow-hidden">
               <Image
                 width="360"
@@ -43,7 +50,7 @@ export default async function List({ params }: { params: { listId: string } }) {
                 src={playlist.items[0].snippet.thumbnails.medium.url}
                 alt={playlist.items[0].snippet.title}
               />
-              <div className="absolute top-0 w-full h-full group-hover:flex bg-black/70 hidden items-center justify-center">
+              <div className="absolute transition ease-in-out duration-300 top-0 w-full h-full group-hover:flex bg-black/70 hidden items-center justify-center">
                 <div className="flex items-center">
                   <Play className="w-5 h-5 text-white" />
                   <h4 className="text-xs font-semibold text-white ml-1">
@@ -56,7 +63,7 @@ export default async function List({ params }: { params: { listId: string } }) {
           <h2 className="text-xl twoLines font-bold mb-4">
             {playlist.items[0].snippet.title}
           </h2>
-          <div className="flex">
+          <div className="flex mb-1">
             <div className="relative group">
               <Link
                 className="text-sm font-semibold"
@@ -75,6 +82,39 @@ export default async function List({ params }: { params: { listId: string } }) {
             <p className="text-xs text-textInput font-medium">
               Created on {formattedDate}
             </p>
+          </div>
+          <div className="flex gap-2 mt-3">
+            <div className="relative group cursor-pointer hover:bg-neutral-50/20 p-2 rounded-full bg-neutral-50/10">
+              <ListPlus size={20} />
+              <div className="opacity-0 whitespace-nowrap group-hover:opacity-100 ease-in duration-100 pointer-events-none top-14 absolute left-0 p-2 rounded-md bg-neutral-600/95">
+                <h4 className="text-xs">Save playlist</h4>
+              </div>
+            </div>
+            <div className="relative group cursor-pointer hover:bg-neutral-50/20 p-2 rounded-full bg-neutral-50/10">
+              <Forward size={20} />
+              <div className="opacity-0 whitespace-nowrap group-hover:opacity-100 ease-in duration-100 pointer-events-none top-14 absolute left-0 p-2 rounded-md bg-neutral-600/95">
+                <h4 className="text-xs">Share</h4>
+              </div>
+            </div>
+            <div className="relative group cursor-pointer hover:bg-neutral-50/20 p-2 rounded-full bg-neutral-50/10">
+              <ArrowDownToLine size={20} />
+              <div className="opacity-0 whitespace-nowrap group-hover:opacity-100 ease-in duration-100 pointer-events-none top-14 absolute left-0 p-2 rounded-md bg-neutral-600/95">
+                <h4 className="text-xs">Download</h4>
+              </div>
+            </div>
+            <div className="cursor-pointer hover:bg-neutral-50/20 p-2 rounded-full bg-neutral-50/10">
+              <MoreVertical size={20} />
+            </div>
+          </div>
+          <div className="flex gap-2 w-full mt-4">
+            <div className="text-black cursor-pointer flex gap-2 justify-center items-center w-full hover:bg-neutral-200 bg-white py-2 rounded-full">
+              <Play size={20} />
+              <h4 className="text-sm font-semibold">Play all</h4>
+            </div>
+            <div className="cursor-pointer flex gap-2 justify-center items-center w-full hover:bg-neutral-50/20 bg-neutral-50/10 py-2 rounded-full">
+              <Shuffle size={20} />
+              <h4 className="text-sm font-semibold">Shuffle</h4>
+            </div>
           </div>
         </div>
       </div>
