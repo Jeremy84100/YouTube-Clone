18940ab601e14107307379993e5793da251861c3
Jeremy84100 <96269524+Jeremy84100@users.noreply.github.com>
2023-12-02 22:35:33 +0100

    search page added

--- app/search/[searchId]/page.tsx
+++ app/search/[searchId]/page.tsx
@@ -1,34 +1,11 @@
 import GetSearchVideos from "@/lib/GetSearchVideo";
 import CardSearch from "@/components/CardSearch";
-import type { Metadata, ResolvingMetadata } from "next";
 
-type Props = {
+export default async function SearchPage({
+  params,
+}: {
   params: { searchId: string };
-};
-
-export async function generateMetadata(
-  { params }: Props,
-  parent: ResolvingMetadata
-): Promise<Metadata> {
-  const searchId = params.searchId;
-
-  // fetch data
-  const product = await fetch(`https://.../${searchId}`).then((res) =>
-    res.json()
-  );
-
-  // optionally access and extend (rather than replace) parent metadata
-  const previousImages = (await parent).openGraph?.images || [];
-
-  return {
-    title: product.title,
-    openGraph: {
-      images: ["/some-specific-page-image.jpg", ...previousImages],
-    },
-  };
-}
-
-export default async function SearchPage({ params }: Props) {
+}) {
   const videosData = GetSearchVideos(params.searchId, null);
   const videoInfo = await videosData;
 
